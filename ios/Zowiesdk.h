2ae6dcdf181ab50b48500b362b12b3bd3822b072
Seth Peterson <seth@sethpeterson.net>
2024-11-29 13:44:53 -0600

    rm new architecture blocks

--- ios/Zowiesdk.h
+++ ios/Zowiesdk.h
@@ -1,12 +1,5 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNZowiesdkSpec.h"
-
-@interface Zowiesdk : NSObject <NativeZowiesdkSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface Zowiesdk : NSObject <RCTBridgeModule>
-#endif
 
 @end
